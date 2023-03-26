7c66a98e87c7e7a947a5264bfa92d5183da424fe
mountainmover <myemail@gmail.com>
2023-03-26 00:31:42 -0700

    map

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _isdigit - checks for a digit
+ * @c: the character to be checked
+ * Return: 1 for digit character or 0 for anything else
  */
-int main(void)
-{
-	char c;
 
-	c = '0';
-	printf("%c: %d\n", c, _isdigit(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isdigit(c));
-	return (0);
+int _isdigit(int c)
+
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+return (0);
 }
