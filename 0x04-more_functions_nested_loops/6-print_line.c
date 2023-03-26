3a6bb602220a693e70b7bb76ea8ea204cb60d5fc
mountainmover <myemail@gmail.com>
2023-03-26 01:24:53 -0700

    we were

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_line -straight -line
+ * @n:no of times
+ * Return:always 0
  */
-int main(void)
-{
-	print_line(0);
-	print_line(2);
-	print_line(10);
-	print_line(-4);
-	return (0);
-}
 
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
