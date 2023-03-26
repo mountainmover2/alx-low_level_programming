3d0bda429777f12f8988fae544914f6117fe7bbe
mountainmover <myemail@gmail.com>
2023-03-26 03:02:58 -0700

    the art of war

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_number - prints an integer
+ * @n:.input integer parameter
  *
- * Return: Always 0.
+ * Return:always 0
  */
-int main(void)
+void print_number(int n)
 {
-	print_number(98);
-	_putchar('\n');
-	print_number(402);
-	_putchar('\n');
-	print_number(1024);
-	_putchar('\n');
-	print_number(0);
-	_putchar('\n');
-	print_number(-98);
-	_putchar('\n');
-	return (0);
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
 }
