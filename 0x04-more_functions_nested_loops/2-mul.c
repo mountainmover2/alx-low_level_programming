2b5b3c7e7c2a525af0823af9a8890782e1416c76
mountainmover <myemail@gmail.com>
2023-03-26 00:42:27 -0700

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * mul - multiplies two integers
+ * @a: - the first integer
+ * @b: - the second integer
+ * Return: always 0
  */
-int main(void)
+
+int mul(int a, int b)
 {
-	printf("%d\n", mul(98, 1024));
-	printf("%d\n", mul(-402, 4096));
-	return (0);
+int result = a * b;
+return (result);
 }
