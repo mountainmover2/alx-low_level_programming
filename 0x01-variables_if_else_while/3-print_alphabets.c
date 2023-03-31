8d8559b601fcde100a1ba4823b1f2a3390e4bbee
mountainmover <myemail@gmail.com>
2023-03-31 05:08:26 -0700

    mcmca

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,37 +1,40 @@
-#include<stdio.h>
-
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: print all possible different
- *           combinations of two digits.
- *
- * Return: Always 0 (Success)
-*/
-
+ * main - main block
+ * Description: Print all possible combinations of two digits.
+ * Numbers must be separated by commas and a space.
+ * 01 and 10 are considered as the same combination of the two digits.
+ * Print only the smallest combination of two digits.
+ * Numbers should be printed in ascending order, with two digits.
+ * You can only use `putchar`.
+ * You can only use `putchar` up to 5 times.
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0
+ */
 int main(void)
 {
-	int digit1 = 0, digit2;
+	int i, j, k;
 
-	while (digit1 <= 9)
+	i = 0;
+
+	while (i < 100)
 	{
-		digit2 = 0;
-		while (digit2 <= 9)
-		{
-			if (digit1 != digit2 && digit1 < digit2)
-			{
-				putchar(digit1 + 48);
-				putchar(digit2 + 48);
+		j = i % 10; /* singles digit */
+		k = i / 10; /* doubles digit */
 
-				if (digit1 + digit2 != 17)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+		if (k < j)
+		{
+			putchar(k + '0');
+			putchar(j + '0');
+
+			if (i < 89)
+			{
+				putchar(44);
+				putchar(32);
 			}
-			++digit2;
 		}
-		++digit1;
+
+		i++;
 	}
 	putchar('\n');
 
