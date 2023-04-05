133edd0d83ff33f371817e543e95406d3209a1c3
mountainmover <myemail@gmail.com>
2023-04-05 03:53:22 -0700

    dream big it may become a reality

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,45 +1,29 @@
+#include "main.h"
+
 /**
- * _sqrt_recursion_wrapper - a wrapper that does the recursion bit
+ * sqrt2 - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
  *
- * @n : input number
- * @min: minimum number to guess
- * @max: maximum number to guess
- *
- * Return: square root of @n or -1
-*/
-int _sqrt_recursion_wrapper(int n, int min, int max)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int sqrt2(int a, int b)
 {
-	int guess, guess_squared;
-
-	guess = (min + max) / 2;		/*get guess (g)*/
-	guess_squared = guess * guess;		/*square guess (sg)*/
-
-	if (guess_squared == n)			/*if squared guess == n return guess*/
-		return (guess);
-	else if (min == max)			/*this means n doesn't have a perfect square*/
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
 		return (-1);
-	else if (guess_squared < n)		/*overshoot g if sg is less than the n*/
-		return (_sqrt_recursion_wrapper(n, guess + 1, max));
-	else					/*undershoot g if sg is more than the n*/
-		return (_sqrt_recursion_wrapper(n, min, guess - 1));
+	return (sqrt2(a, b + 1));
 }
-
 /**
- * _sqrt_recursion - a function that returns the natural
- *                   square root of a number
+ * _sqrt_recursion - returns the natural square root of n
+ * @n: Number Integer
  *
- * @n: input number
- *
- * Return: square root
-*/
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _sqrt_recursion(int n)
 {
-	if (n == 1)		/*sqrt(1) == 1*/
-		return (1);
-	else if (n == 0)	/*sqrt(0) == 0*/
-		return (0);
-	else if (n < 0)		/*sqrt(-n) == -1 (NA)*/
-		return (-1);
-	else
-		return (_sqrt_recursion_wrapper(n, 1, n));
+	return (sqrt2(n, 1));
 }
